481d838ca8f855595244a4a403f97b7ebee5be7b
juan-mfernandes <mfernandes.juan@gmail.com>
2024-11-06 17:52:01 -0300

    add migration V1: create-table-clients

---
+++ src/main/resources/db/migration/V1__create-table-clientes.sql
@@ -0,0 +1,6 @@
+CREATE TABLE clientes (
+    id int not null primary key auto_increment,
+    nome varchar(200),
+    email varchar(500),
+    telefone varchar(11)
+);
---
+++ src/test/java/br/grupointegrado/lanches/LanchesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.grupointegrado.lanches;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LanchesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
