b0d4fad8e3b42da1a1e28e6487151af8ebbc8629
juan-mfernandes <mfernandes.juan@gmail.com>
2024-11-07 11:14:13 -0300

    add findById

--- src/main/java/br/grupointegrado/lanches/controller/ClienteController.java
+++ src/main/java/br/grupointegrado/lanches/controller/ClienteController.java
@@ -4,19 +4,29 @@ import br.grupointegrado.lanches.model.Cliente;
 import br.grupointegrado.lanches.repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/clientes")
 public class ClienteController {
 
     @Autowired
     private ClienteRepository repository;
 
-    @GetMapping("/clientes")
+    @GetMapping
     public List<Cliente> findAll() {
         return this.repository.findAll();
+    }
+
+    // @PathVariable é usado para freezar que o parâmetro "id" será passado na URL da requisição
+    @GetMapping("/{id}")
+    public Cliente findById(@PathVariable Integer id) {
+        return this.repository.findById(id).
+                orElseThrow( () -> new IllegalArgumentException("Cliente não encontrado."));
     }
 
 }
